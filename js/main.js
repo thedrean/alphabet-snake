3716f3b0d5ccbb308430296542d0a3f616ef9651
louisfettet <louisfettet@gmail.com>
2014-03-22 12:18:38 -0400

    Fixed up index and added js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var cellWidth = 64;
+var cellHeight = 64;
+
+$(document).ready(function(){
+	$("#canvas").height = $(window).height();
+	$("#canvas").width = $(window).width();
+	
+	var Grid = {};
+
+});
+
