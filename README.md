a953dd12a97f3dea4e211c38d0e94c228afa814b
ally palanzi <ampalanzi@gmail.com>
2014-03-22 13:44:38 -0400

    gh pages woo

---
+++ README.md
@@ -0,0 +1,13 @@
+Alphabet Snake
+==============
+
+Web App for toddlers 
+
+## Workflow with git for the newbies:
+
++ Make changes to a file named `<filename>` (example, obvy)
++ Save changes
++ In the terminal, make sure you are in the directory of the repo (`alphabet-snake`)
++ Do `git add <filename>`.  You may have to do the full path to the filename, like `git add js/main.js` for example
++ Think of an informative message, like `changed a bunch of stuff in main.js`, and do `git commit -m "changed a bunch of stuff in main.js"`
++ Last, do `git push origin master`.  If you don't see anything that says `error` or `abort` or another scary word, it probably worked!
---
+++ js/main.js
@@ -0,0 +1,83 @@
+var cell_dim = 64               // cells are squares, only need one dimension
+$(document).ready(function(){
+
+	var windowH = $(window).height();
+	var windowW = $(window).width();
+
+	$("#canvas").attr({
+		height: windowH,
+		width: windowW
+	});
+
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+
+	// Event listeners for canvas...
+	canvas.addEventListener("mousedown", doSomething);
+    canvas.addEventListener("mousemove", doSomething);
+    canvas.addEventListener("touchstart", doSomething);
+    canvas.addEventListener("touchmove", doSomething);
+    canvas.addEventListener("touchend", doSomething);
+
+	
+	var Grid = {};
+    Grid.width = windowW/cell_dim;
+    Grid.height = windowH/cell_dim;
+    DrawLine(ctx);
+    
+    var Snake = {body:[{x:0, y:0}], 
+                 direction:{x:1, y:0}}
+    Grid[0] = {0:"snake"}
+    function animationLoop(){
+        step(Snake, Grid)
+    }
+    
+
+});
+function DrawLine(ctx){
+	ctx.moveTo(0,0);
+	ctx.lineTo(0, 768);
+	ctx.moveTo(0,0);
+	ctx.lineTo(1024, 0);
+	ctx.moveTo(1024,0);
+	ctx.lineTo(1024, 768);
+	ctx.moveTo(1024,768);
+	ctx.lineTo(0, 768);
+
+	ctx.stroke();
+}
+
+function doSomething() {
+	console.log("did something");
+}
+
+
+
+function step(snake, grid){
+    var newhead = {x: snake.body[0].x+direction.x, y:snake.body[0].y+direction.y}
+    
+    if (newhead.x >= 0 && newhead.x < grid.width && newhead.y >= 0 && newhead.y < grid.height)
+        ;
+    else{
+        // You lose/game over or whatever
+    }
+        
+    if (grid[newhead.x] && grid[newhead.x][newhead.y]){
+        var obj = grid[newhead.x][newhead.y];
+        if (obj == "letter"){
+            // eat it
+        }else{
+            // You lose
+        }
+    }else{
+        var head = snake.body[0]
+        var tail = snake.body.pop()
+        
+        grid[tail.x][tail.y] = undefined
+        
+        tail = {x:head.x+direction.x, y:head.y+direction.y}
+        snake.body.unshift(tail)
+        
+        grid[tail.x][tail.y] = "snake"
+    }
+}
