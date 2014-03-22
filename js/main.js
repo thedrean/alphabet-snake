a1054c67f0579fbf713727c17140509e3ea54056
Matt Havard <mh6445a@american.edu>
2014-03-22 12:21:15 -0400

    grid sizing

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var cellWidth = 64;
-var cellHeight = 64;
-
+var cell_dim = 64               // cells are squares, only need one dimension
 $(document).ready(function(){
 	$("#canvas").height = $(window).height();
 	$("#canvas").width = $(window).width();
 	
 	var Grid = {};
+    Grid.width = $(window).width()/cell_dim
+    Grid.height = $(window).height()/cell_dim
 
 });
 
